f6714a34e9b9ccb6b89ea1af08f89e1aa79cd560
stefano-lev <stefano-lev@protonmail.com>
2025-01-17 21:12:16 -0600

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/steflev-portfolio">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
